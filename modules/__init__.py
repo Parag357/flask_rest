119f1aeb4e9df40379fff72051322169312d0c3b
Parag357 <psychoprg357@gmail.com>
2020-10-22 11:36:54 +0530

    unnecessary code removed

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,10 +1,5 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-#from marshmallow import Schema, fields
-#from marshmallow_sqlalchemy import SQLAlchemySchema, auto_field
-#import os
-
-
 
 app=Flask(__name__) #app initialistaion
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres@localhost/inventory'
