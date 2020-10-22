dc2dc7d4210ccc5e0e3561d877e4073c5198038b
Parag357 <psychoprg357@gmail.com>
2020-10-22 18:38:48 +0530

    optimisations based on todays discussion

--- test.py
+++ test.py
@@ -1,2 +1,19 @@
-from modules import app
-import unittest
+from flask import Flask, request, jsonify
+from modules.endpoints import configure_routes
+
+def test_delete_success():
+	app=Flask(__name__)
+	configure_routes(app)
+	client=app.test_client()
+	url='/create'
+
+	mock_data={
+	"name":"prod10",
+	"price":"23.5",
+	"expiry":"03/03/2026",
+    "category_id":1
+	}
+
+	response=client.post(url,data=json.dump(mock_data))
+	assert response.status_code == 200
+
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 from modules import app
+from modules.endpoints import configure_routes
 
 
+configure_routes(app)
 
 if __name__ == '__main__':
 	app.run()
